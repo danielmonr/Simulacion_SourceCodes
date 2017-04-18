b4f10fcdc233f1124240c9045dc68bde9603728f
Monzalvo <danielmonr@gmail.com>
2017-04-18 09:32:01 -0500

    Colas

--- colas/MMi.cpp
+++ colas/MMi.cpp
@@ -19,6 +19,71 @@ float Po(int m, float l, float u){
   return 1/res;
 }
 
+float cost(int m, float l, float w, float Cs, float Cw){
+  return (Cs*m) + (l*w*Cw);
+}
+
+float single(float l, float u){
+  cout << "\n\tl =" << l<< "; u = " << u << endl;
+  float w = 1/(u-l);
+  cout << "\tL = " << l/(u-l) << endl;
+  cout << "\tW = " << w << endl;
+  cout << "\tLq = " << l*l/(u*(u-l)) << endl;
+  cout << "\tWq = " << l/(u*(u-l)) << endl;
+  cout << "\tp = " << l/u << endl;
+  cout << "\tp0 = " << 1- (l/u) << endl;
+  return w;
+}
+
+void single_eval(float l, float u){
+  int option;
+  float min_cost;
+  float temp_cost;
+  float Cs, Cw;
+  cout << "Cost of one clerk: " << endl;
+  cin >> Cs;
+  cout << "Cost of Wait: " << endl;
+  cin >> Cw;
+  int k = 1;
+  cout << "How many clerks? (0 to calculate better option.) ";
+  cin >> option;
+  switch (option) {
+    case 0:
+      while(u*k < l)
+        k++;
+      cout << "Minimum number of clerks: " << k << endl;
+      min_cost = cost(k, l, single(l, u*k), Cs, Cw);
+      k++;
+      while (cost(k, l, single(l, u*k), Cs, Cw) < min_cost) {
+        min_cost = cost(k, l, single(l, u*k), Cs, Cw);
+        cout << "K => " << k << '\t';
+        cout << "\tCost =" <<  min_cost << "\n";
+        k++;
+      }
+      cout << "\nMin cost = " << min_cost << " for k = " << k-1 << endl << endl;
+      break;
+    default:
+      cout << "Cost for " << option << " clerks = " << cost(option, l, single(l, u*option), Cs, Cw) << ".\n";
+      break;
+  }
+}
+
+void multiple(int m, float l, float u){
+  float p = Po(m,l,u);
+  float L = ((l*u*pow(l/u,m)) / (fact(m-1)*(m*u-l)*(m*u-l)))*p + (l/u);
+
+  cout << "L= " << L << endl;
+  cout << "W= " << L/l << endl;
+  cout << "Lq= " << L - (l/u) << endl;
+  cout << "Wq= " << (L/l) - (1/u) << endl;
+  cout << "p= " << l / (m*u) << endl;
+  cout << "p0= " << Po(m,l,u) << endl;
+}
+
+void multiple_eval(m, l, u){
+  cout << "Multiple eval:\n";
+}
+
 int main(int argc, char* argv[]){
     if(argc != 3){
         cout << "Parameters arrivals units_served\n";
@@ -32,19 +97,22 @@ int main(int argc, char* argv[]){
     cout << "u = " << u << endl;
 
     int m;
-    while (1){
+    //while (1){
         cout << "servers" << endl;
         cin >> m;
-        float p = Po(m,l,u);
-        float L = ((l*u*pow(l/u,m)) / (fact(m-1)*(m*u-l)*(m*u-l)))*p + (l/u);
+        switch (m) {
+          case 0:
+            all_eval(l, u);
+            break;
+          case 1:
+            single_eval(l,u);
+            break;
+          default:
+            multiple(m, l, u);
+            break;
+        }
 
-        cout << "L= " << L << endl;
-        cout << "W= " << L/l << endl;
-        cout << "Lq= " << L - (l/u) << endl;
-        cout << "Wq= " << (L/l) - (1/u) << endl;
-        cout << "p= " << l / (m*u) << endl;
-        cout << "p0= " << Po(m,l,u) << endl;
-    }
+    //}
 
     return 1;
 }
